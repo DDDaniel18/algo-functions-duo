b955552c93c4def3b2b22e69743defded8e29d66
DDDaniel18 <danieldeutch15@gmail.com>
2025-06-19 14:37:40 +0300

    added factorial

--- math_functions.py
+++ math_functions.py
@@ -6,5 +6,11 @@
 def welcome():
     print("welcome to the library")
 
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        reutrn n * factorial(n - 1)
 if __name__ == "__main__":
     welcome()
+    print(factorial(5))
