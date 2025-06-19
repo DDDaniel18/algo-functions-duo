e3954cd2c9adea341e5c90992069626fc84af699
DDDaniel18 <danieldeutch15@gmail.com>
2025-06-19 14:40:31 +0300

    merge is prime

--- math_functions.py
+++ math_functions.py
@@ -11,6 +11,18 @@ def factorial(n):
         return 1
     else:
         reutrn n * factorial(n - 1)
+
+def isPrime(n, div):
+    if n < 2:
+        return False
+    if n % div == 0:
+        return False
+    if div == 1:
+        return True
+    return isPrime(n, div - 1)
+
+
 if __name__ == "__main__":
     welcome()
     print(factorial(5))
+    print(isPrime(11))
